78a320befa28c7e33f71ee971fd3c32ade2f2b24
Blackrush <blackrushx@gmail.com>
2014-06-07 17:32:39 +0200

    new @Listener annotation

---
+++ api/src/main/java/com/github/blackrush/acara/Listener.java
@@ -0,0 +1,13 @@
+package com.github.blackrush.acara;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Listener {
+    boolean disabled() default false;
+}
