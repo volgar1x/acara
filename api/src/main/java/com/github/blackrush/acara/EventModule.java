78951d9f0a890bf049f5831d7cc05a8dfde5a696
Blackrush <blackrushx@gmail.com>
2014-06-11 01:01:01 +0200

    issue #9: EventModule is composable

--- api/src/main/java/com/github/blackrush/acara/EventModule.java
+++ api/src/main/java/com/github/blackrush/acara/EventModule.java
@@ -3,4 +3,22 @@ package com.github.blackrush.acara;
 @FunctionalInterface
 public interface EventModule {
     EventBusBuilder configure(EventBusBuilder builder);
+
+    default EventModule andThen(EventModule other) {
+        return builder -> other.configure(this.configure(builder));
+    }
+
+    default EventModule compose(EventModule other) {
+        return builder -> this.configure(other.configure(builder));
+    }
+
+    static final EventModule UNIT = builder -> builder;
+
+    public static EventModule unit() {
+        return UNIT;
+    }
+
+    public static EventModule unitModule() {
+        return unit();
+    }
 }
