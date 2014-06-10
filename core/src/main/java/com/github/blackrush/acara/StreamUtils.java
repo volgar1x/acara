ae5e46a8cc3a13d3fa7ea66701534e23bad5d3a2
Blackrush <blackrushx@gmail.com>
2014-06-10 17:12:36 +0200

    some improvements and new methods

--- core/src/main/java/com/github/blackrush/acara/StreamUtils.java
+++ core/src/main/java/com/github/blackrush/acara/StreamUtils.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 import java.util.Optional;
 import java.util.Spliterator;
 import java.util.Spliterators;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -36,27 +35,36 @@ final class StreamUtils {
         return opt.map(Stream::of).orElse(Stream.empty());
     }
 
-    public static Stream<Class<?>> traverseInheritance(Class<?> klass) {
-        if (klass == Object.class) {
-            return Stream.empty();
-        }
-
-        return StreamSupport.stream(new Spliterators.AbstractSpliterator<Class<?>>(Long.MAX_VALUE, 0) {
-            Class<?> cur = klass;
+    public static <T> Stream<T> iterate(Optional<T> seed, Function<T, Optional<T>> fn) {
+        return StreamSupport.stream(new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, 0) {
+            Optional<T> cur = seed;
 
             @Override
-            public boolean tryAdvance(Consumer<? super Class<?>> action) {
-                action.accept(cur);
-
-                Class<?> parent = cur.getSuperclass();
-                if (parent == Object.class) {
-                    return false;
-                } else {
-                    cur = parent;
-                    return true;
-                }
+            public boolean tryAdvance(Consumer<? super T> action) {
+                cur.ifPresent(action);
+                cur = cur.flatMap(fn);
+                return cur.isPresent();
             }
         }, false);
+    }
+
+    public static <T> Stream<T> collect(Stream<T> seed, Function<T, Stream<T>> fn) {
+        return seed.<T>flatMap(it -> Stream.concat(Stream.of(it), collect(fn.apply(it), fn)));
+    }
+
+    public static Stream<Class<?>> traverseInheritance(Class<?> klass) {
+        return traverseInheritance(klass, Object.class);
+    }
+
+    public static Stream<Class<?>> traverseInheritance(Class<?> klass, Class<?> limit) {
+        return klass == limit ? Stream.empty() : iterate(Optional.of(klass), it -> optionalParent(it, limit));
+    }
+
+    public static Optional<Class<?>> optionalParent(Class<?> klass, Class<?> limit) {
+        return klass.getSuperclass() == limit
+                ? Optional.empty()
+                : Optional.of(klass.getSuperclass())
+                ;
     }
 
     public static Stream<Class<?>> directParent(Class<?> klass, Class<?> limit) {
@@ -65,16 +73,5 @@ final class StreamUtils {
             return Stream.empty();
         }
         return Stream.of(superclass);
-    }
-
-    public static <T, U, R> Stream<R> combination(T a, U b, Function<T, Stream<T>> fnR, Function<U, Stream<U>> fnU, BiFunction<T, U, R> fnT) {
-        Stream<T> aa = fnR.apply(a);
-        Stream<U> bb = fnU.apply(b);
-
-        return Stream.<Stream<R>>of(
-                aa.map(x -> fnT.apply(x, b)),
-                bb.map(x -> fnT.apply(a, x)),
-                aa.flatMap(xA -> bb.map(xB -> fnT.apply(xA, xB)))
-        ).reduce(Stream.empty(), Stream::concat);
     }
 }
