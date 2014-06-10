ea5006112e1a4b6c02879c60e71603a3a22b72d3
Blackrush <blackrushx@gmail.com>
2014-06-10 10:37:24 +0200

    create SuperviseDispatcher lookup

--- core/src/main/java/com/github/blackrush/acara/SuperviseDispatcher.java
+++ core/src/main/java/com/github/blackrush/acara/SuperviseDispatcher.java
@@ -1,16 +1,28 @@
 package com.github.blackrush.acara;
 
-import com.github.blackrush.acara.ListenerMetadata;
+import com.github.blackrush.acara.dispatch.DispatcherLookup;
 import com.github.blackrush.acara.dispatch.TypedDispatcher;
 import com.github.blackrush.acara.supervisor.event.SupervisedEvent;
 import org.fungsi.Either;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
+import static com.github.blackrush.acara.SuperviseListenerMetadataLookup.isSuperviseListener;
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@inheritDoc}
  */
 public final class SuperviseDispatcher extends TypedDispatcher<SupervisedEvent> {
+    public static final DispatcherLookup LOOKUP = metadata -> {
+        if (isSuperviseListener(metadata.getListenerMethod())) {
+            return Optional.of(new SuperviseDispatcher(metadata));
+        }
+
+        return Optional.empty();
+    };
+
     private final ListenerMetadata metadata;
 
     /**
@@ -18,7 +30,7 @@ public final class SuperviseDispatcher extends TypedDispatcher<SupervisedEvent> 
      * @param metadata non-null value
      */
     public SuperviseDispatcher(ListenerMetadata metadata) {
-        this.metadata = metadata;
+        this.metadata = requireNonNull(metadata, "metadata");
     }
 
     @Override
