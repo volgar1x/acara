f78b10ad8beb2048f578a14eddf2f2535b5d2ccb
Blackrush <blackrushx@gmail.com>
2014-06-11 14:01:03 +0200

    add handy getters to EventBusBuilder

--- api/src/main/java/com/github/blackrush/acara/EventBusBuilder.java
+++ api/src/main/java/com/github/blackrush/acara/EventBusBuilder.java
@@ -17,11 +17,23 @@ public interface EventBusBuilder {
     }
 
     /**
+     * Get current {@link com.github.blackrush.acara.ListenerMetadataLookup} which might be used by the final building {@link com.github.blackrush.acara.EventBus}
+     * @return a non-null lookup
+     */
+    ListenerMetadataLookup getMetadataLookup();
+
+    /**
      * Concat current {@link com.github.blackrush.acara.ListenerMetadataLookup} with another one.
      * @param metadataLookup a non-null lookup
      * @return a non-null builder
      */
     EventBusBuilder addMetadataLookup(ListenerMetadataLookup metadataLookup);
+
+    /**
+     * Get current {@link com.github.blackrush.acara.dispatch.DispatcherLookup} which might be used by the final building {@link com.github.blackrush.acara.EventBus}
+     * @return a non-null lookup
+     */
+    DispatcherLookup getDispatcherLookup();
 
     /**
      * Aggregate current {@link com.github.blackrush.acara.dispatch.DispatcherLookup} with another one through
@@ -32,12 +44,24 @@ public interface EventBusBuilder {
     EventBusBuilder addDispatcherLookup(DispatcherLookup dispatcherLookup);
 
     /**
+     * Get current {@link com.github.blackrush.acara.EventMetadataLookup} which might be used by the final building {@link com.github.blackrush.acara.EventBus}
+     * @return a non-null lookup
+     */
+    EventMetadataLookup getEventMetadataLookup();
+
+    /**
      * Aggregate current {@link com.github.blackrush.acara.EventMetadataLookup} with another one through
      * {@link com.github.blackrush.acara.EventMetadataLookup#withFallback(EventMetadataLookup)}
      * @param eventMetadataLookup a non-null lookup
      * @return a non-null builder
      */
     EventBusBuilder addEventMetadataLookup(EventMetadataLookup eventMetadataLookup);
+
+    /**
+     * Get current {@link com.github.blackrush.acara.supervisor.Supervisor} which might be used by the final building {@link com.github.blackrush.acara.EventBus}
+     * @return a non-null supervisor
+     */
+    Supervisor getSupervisor();
 
     /**
      * Set current {@link com.github.blackrush.acara.supervisor.Supervisor}. This method is destructive as the previous
