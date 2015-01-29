7a3b5d050e0360f5b49162c4ab08eaf081ba8587
Blackrush <blackrushx@gmail.com>
2015-01-29 18:34:12 +0100

    Add Javadoc to Listen annotation

--- api/src/main/java/com/github/blackrush/acara/Listen.java
+++ api/src/main/java/com/github/blackrush/acara/Listen.java
@@ -6,8 +6,15 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Mark a method as an event listener.
+ */
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface Listen {
+    /**
+     * Enable or disable this method. {@code false} by default.
+     * @return whether or not the method is disabled
+     */
     boolean disabled() default false;
 }
