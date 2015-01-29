089b0c09ce0141f8fbdd6d71b0edce85595161f9
Blackrush <blackrushx@gmail.com>
2015-01-29 18:32:21 +0100

    Asynchronously publish every time

--- api/src/main/java/com/github/blackrush/acara/Publishable.java
+++ api/src/main/java/com/github/blackrush/acara/Publishable.java
@@ -10,19 +10,6 @@ import java.util.List;
  * @see com.github.blackrush.acara.EventBus
  */
 public interface Publishable {
-    /**
-     * Publish asynchronously an event. This method is therefore not blocking.
-     * @param event a non-null event
-     * @return a non-null {@link org.fungsi.concurrent.Future} of {@link java.util.List} of {@link java.lang.Object} representing answers of listeners
-     */
-    Future<List<Object>> publishAsync(Object event);
-
-    /**
-     * Publish synchronously an event. This method is therefore actually blocking.
-     * @param event a non-null event
-     * @return a non-null {@link java.util.List} of {@link java.lang.Object} representing answsers of listeners.
-     */
-    List<Object> publishSync(Object event);
 
     /**
      * Publish an event. This method may be asynchronous or synchronous, depending on implementation defaults.
