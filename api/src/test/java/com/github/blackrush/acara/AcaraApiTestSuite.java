bec4458fe8efeb22504f77d0403d2fac0156a3ce
Blackrush <blackrushx@gmail.com>
2015-01-29 18:25:58 +0100

    Dust off acara-api

--- api/src/test/java/com/github/blackrush/acara/AcaraApiTestSuite.java
+++ api/src/test/java/com/github/blackrush/acara/AcaraApiTestSuite.java
@@ -1,12 +1,10 @@
 package com.github.blackrush.acara;
 
-import com.github.blackrush.acara.dispatch.TypedDispatcherTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TypedDispatcherTest.class
 })
 public class AcaraApiTestSuite {
 }
