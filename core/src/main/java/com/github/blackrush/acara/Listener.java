b12e50a4d4273aeda21a0da556e52ebc0d48a9af
Blackrush <blackrushx@gmail.com>
2015-01-30 10:46:13 +0100

    Improve Javadoc & Give a worker when dispatching a Listener

--- core/src/main/java/com/github/blackrush/acara/Listener.java
+++ core/src/main/java/com/github/blackrush/acara/Listener.java
@@ -1,18 +1,20 @@
 package com.github.blackrush.acara;
 
 import org.fungsi.concurrent.Future;
+import org.fungsi.concurrent.Worker;
 
 public abstract class Listener {
     /**
-     *
-     * @return
+     * A Listener handles one and only one type of event
+     * @return the non-null event's metadata
      */
     public abstract EventMetadata getHandledEvent();
 
     /**
      * Immediately dispatch an event.
      * @param event a non-null event
+     * @param worker a non-null worker
      * @return the pending response
      */
-    public abstract Future<Object> dispatch(Object event);
+    public abstract Future<Object> dispatch(Object event, Worker worker);
 }
