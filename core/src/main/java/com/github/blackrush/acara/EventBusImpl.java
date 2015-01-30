bd06e2a8a9ae464082246f60ad07ed02d6d9f069
Blackrush <blackrushx@gmail.com>
2015-01-30 10:46:35 +0100

    Second batch of EventBus' core implementation

--- core/src/main/java/com/github/blackrush/acara/EventBusImpl.java
+++ core/src/main/java/com/github/blackrush/acara/EventBusImpl.java
@@ -1,6 +1,8 @@
 package com.github.blackrush.acara;
 
 import org.fungsi.concurrent.Future;
+import org.fungsi.concurrent.Futures;
+import org.fungsi.concurrent.Worker;
 
 import java.util.Collection;
 import java.util.Deque;
@@ -8,60 +10,54 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.locks.StampedLock;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class EventBusImpl implements EventBus {
     final EventMetadataBuilder eventMetadataBuilder;
     final ListenerBuilder listenerBuilder;
+    final Worker worker;
 
-    private class Listeners {
-        Deque<Listener> list = new ConcurrentLinkedDeque<>();
-        StampedLock lock = new StampedLock();
-    }
-    private final Map<EventMetadata, Listeners> listeners = new ConcurrentHashMap<>();
+    private final Map<EventMetadata, Deque<Listener>> listeners
+            = new ConcurrentHashMap<>();
 
-    EventBusImpl(EventMetadataBuilder eventMetadataBuilder, ListenerBuilder listenerBuilder) {
+    EventBusImpl(EventMetadataBuilder eventMetadataBuilder, ListenerBuilder listenerBuilder, Worker worker) {
         this.eventMetadataBuilder = eventMetadataBuilder;
         this.listenerBuilder = listenerBuilder;
-    }
-
-    private Listeners subscribe(Listener listener, EventMetadata evt) {
-        Listeners l = listeners.computeIfAbsent(evt, x -> new Listeners());
-        long stamp = l.lock.writeLock();
-        try {
-            l.list.add(listener);
-            return l;
-        } finally {
-            l.lock.unlockWrite(stamp);
-        }
-    }
-
-    @Override
-    public Subscription subscribe(Object subscriber) {
-        Listener listener = listenerBuilder.build(subscriber);
-
-        Listeners l = subscribe(listener, listener.getHandledEvent());
-
-        return () -> {
-            long stamp = l.lock.writeLock();
-            try {
-                l.list.remove(listener);
-            } finally {
-                l.lock.unlockWrite(stamp);
-            }
-        };
-    }
-
-    @Override
-    public Subscription subscribeMany(Collection<?> subscribers) {
-        for (Object subscriber : subscribers) {
-            Listener listener = listenerBuilder.build(subscriber);
-
-        }
+        this.worker = worker;
     }
 
     @Override
     public Future<List<Object>> publish(Object event) {
-        return null;
+        EventMetadata metadata = eventMetadataBuilder.build(event);
+        Deque<Listener> deq = listeners.get(metadata);
+        return deq.stream()
+                .map(x -> x.dispatch(event, worker))
+                .collect(Futures.collect());
+    }
+
+    Subscription subscribe(Stream<Listener> stream) {
+        Map<EventMetadata, List<Listener>> l = stream.collect(
+            Collectors.groupingBy(Listener::getHandledEvent));
+
+        l.forEach((metadata, list) -> {
+            Deque<Listener> deq = listeners.computeIfAbsent(metadata, x -> new ConcurrentLinkedDeque<>());
+            deq.addAll(list);
+        });
+
+        return () ->
+            l.forEach((metadata, list) ->
+                listeners.get(metadata).removeAll(list)
+            );
+    }
+
+    @Override
+    public Subscription subscribe(Object sub) {
+        return subscribe(listenerBuilder.build(sub));
+    }
+
+    @Override
+    public Subscription subscribeMany(Collection<?> subs) {
+        return subscribe(subs.stream().flatMap(listenerBuilder::build));
     }
 }
