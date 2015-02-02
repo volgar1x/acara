04d538fdcff7257aee3965bf471307451d0e273e
Blackrush <blackrushx@gmail.com>
2015-02-02 18:10:43 +0100

    JavaEventMetadata do know its event's type

--- core-java/src/main/java/com/github/blackrush/acara/JavaEventMetadata.java
+++ core-java/src/main/java/com/github/blackrush/acara/JavaEventMetadata.java
@@ -1,25 +1,28 @@
 package com.github.blackrush.acara;
 
-public final class JavaEventMetadata extends EventMetadata {
-    final Class<?> klass;
+public final class JavaEventMetadata<T> extends TypedEventMetadata<T> {
+    final Class<T> klass;
 
-    public JavaEventMetadata(Class<?> klass) {
+    public JavaEventMetadata(Class<T> klass) {
         this.klass = klass;
     }
 
     @Override
-    public EventMetadata getParent() {
-        if (klass.getSuperclass() == Object.class) {
+    public JavaEventMetadata<? super T> getParent() {
+        Class<? super T> superclass = klass.getSuperclass();
+
+        if (superclass == Object.class) {
             return null;
         }
-        return new JavaEventMetadata(klass.getSuperclass());
+
+        return new JavaEventMetadata<>(superclass);
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || o.getClass() != this.getClass()) return false;
         if (o == this) return true;
-        JavaEventMetadata other = (JavaEventMetadata) o;
+        JavaEventMetadata<?> other = (JavaEventMetadata<?>) o;
         return other.klass.equals(this.klass);
     }
 
--- core-java/src/main/java/com/github/blackrush/acara/JavaEventMetadataBuilder.java
+++ core-java/src/main/java/com/github/blackrush/acara/JavaEventMetadataBuilder.java
@@ -3,6 +3,6 @@ package com.github.blackrush.acara;
 public final class JavaEventMetadataBuilder implements EventMetadataBuilder {
     @Override
     public EventMetadata build(Object event) {
-        return new JavaEventMetadata(event.getClass());
+        return new JavaEventMetadata<>(event.getClass());
     }
 }
