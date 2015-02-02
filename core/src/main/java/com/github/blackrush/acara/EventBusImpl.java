e9cb29646ea42f0c742ccacb995f67419dacd529
Blackrush <blackrushx@gmail.com>
2015-02-02 11:09:47 +0100

    Fix event bubbling

--- core/src/main/java/com/github/blackrush/acara/EventBusImpl.java
+++ core/src/main/java/com/github/blackrush/acara/EventBusImpl.java
@@ -1,25 +1,28 @@
 package com.github.blackrush.acara;
 
-import com.google.common.collect.ImmutableList;
 import org.fungsi.concurrent.Future;
 import org.fungsi.concurrent.Futures;
 import org.fungsi.concurrent.Worker;
 
 import java.util.Collection;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 final class EventBusImpl implements EventBus {
     final EventMetadataBuilder eventMetadataBuilder;
     final ListenerBuilder listenerBuilder;
     final Worker worker;
 
-    private final Map<EventMetadata, Deque<Listener>> listeners
+    /* state => (signature => behavior)
+             ^             ^
+             |             |
+           rw m/t         ro            */
+    private final ConcurrentMap<Object, Map<EventMetadata, Listener>> listeners
             = new ConcurrentHashMap<>();
 
     EventBusImpl(EventMetadataBuilder eventMetadataBuilder, ListenerBuilder listenerBuilder, Worker worker) {
@@ -28,41 +31,51 @@ final class EventBusImpl implements EventBus {
         this.worker = worker;
     }
 
-    @Override
-    public Future<List<Object>> publish(Object event) {
-        EventMetadata metadata = eventMetadataBuilder.build(event);
-        Deque<Listener> deq = listeners.get(metadata);
-        if (deq == null || deq.isEmpty()) {
-            return Futures.success(ImmutableList.of());
+    private Listener lookupListener(EventMetadata meta, Map<EventMetadata, Listener> listeners) {
+        Listener listener = null;
+        for (EventMetadata cur = meta; listener == null && cur != null; cur = cur.getParent()) {
+            listener = listeners.get(cur);
         }
-
-        return deq.stream()
-                .map(x -> x.dispatch(event, worker))
-                .collect(Futures.collect());
+        return listener;
     }
 
-    Subscription subscribe(Stream<Listener> stream) {
-        Map<EventMetadata, List<Listener>> l = stream.collect(
-            Collectors.groupingBy(Listener::getHandledEvent));
+    private Map<EventMetadata, Listener> buildListeners(Object sub) {
+        return listenerBuilder.build(sub)
+                .collect(Collectors.toMap(
+                        Listener::getHandledEvent,
+                        Function.identity()));
+    }
 
-        l.forEach((metadata, list) -> {
-            Deque<Listener> deq = listeners.computeIfAbsent(metadata, x -> new ConcurrentLinkedDeque<>());
-            deq.addAll(list);
+    @Override
+    public Future<List<Object>> publish(Object event) {
+        EventMetadata meta = eventMetadataBuilder.build(event);
+
+        // there are, at most, as many responses as there are subscribers
+        // in reality, we hardly ever get as many responses
+        List<Future<Object>> results = new LinkedList<>();
+
+        listeners.forEach((sub, listeners) -> {
+            Listener listener = lookupListener(meta, listeners);
+            if (listener != null) {
+                Future<Object> result = listener.dispatch(event, worker);
+                results.add(result);
+            }
         });
 
-        return () ->
-            l.forEach((metadata, list) ->
-                listeners.get(metadata).removeAll(list)
-            );
+        return Futures.collect(results);
     }
 
     @Override
     public Subscription subscribe(Object sub) {
-        return subscribe(listenerBuilder.build(sub));
+        listeners.putIfAbsent(sub, buildListeners(sub));
+        return () -> listeners.remove(sub);
     }
 
     @Override
     public Subscription subscribeMany(Collection<?> subs) {
-        return subscribe(subs.stream().flatMap(listenerBuilder::build));
+        for (Object sub : subs) {
+            listeners.putIfAbsent(sub, buildListeners(sub));
+        }
+        return () -> subs.forEach(listeners::remove);
     }
 }
